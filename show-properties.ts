64bb984ca9e85290b9bf1adbcafdf92a127cf2c6
Vikram <vikramtheone1@gmail.com>
2015-08-14 21:29:57 -0700

    somewhere in wild

---
+++ show-properties.ts
@@ -0,0 +1,25 @@
+// TypeScript
+import {Component, View, bootstrap, NgFor} from 'angular2/angular2';
+@Component({
+  selector: 'display'
+})
+@View({
+  template: `
+   <p>My name: {{ myName }}</p>
+   <p>Friends:</p>
+    <ul>
+       <li *ng-for="#name of names">
+          {{ name }}
+       </li>
+    </ul>
+  `,
+  directives: [NgFor]
+})
+class DisplayComponent {
+  myName: string;
+  names: Array<string>;
+  constructor() {
+    this.myName = "Alice";
+    this.names = ["Aarav", "Martín", "Shannon", "Ariana", "Kai"];
+  }
+}
